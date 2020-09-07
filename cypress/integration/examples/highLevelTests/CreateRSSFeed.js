d3d1fdcbf061ffc8c84254562b3b5d92e3d0ad7b
Rashi Gadia <rashi.gadia@office.pur3.net>
2020-09-07 11:18:13 +0100

    Amended SMS and RSS creation script

--- cypress/integration/examples/highLevelTests/CreateRSSFeed.js
+++ cypress/integration/examples/highLevelTests/CreateRSSFeed.js
@@ -8,7 +8,7 @@ describe('RSS Feed Creation', function () {
       cy.get('.type-switcher-dropdown > .btn').click();
       cy.findByText('RSS feeds').click();
       cy.contains('button', 'New RSS feed').click(  );
-      cy.get('[p3-validator-bootstrap="rss-feed-name"] > .countdown-group > .form-control').type('RSS Feed').type(Date.now());
+      cy.get('[name="rss-feed-name"]').type('RSS Feed').type(Date.now());
       cy.get('[p3-validator-bootstrap="rss-feed-uri"] > .countdown-group > .form-control').type('	http://feeds.bbci.co.uk/news/rss.xml');
       cy.contains('button', 'Save and finish').click()
     });
